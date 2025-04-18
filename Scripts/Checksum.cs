df882082574de98c1a036d23065d65b5c2df8f7e
smudge <goddessconduit@gmail.com>
2025-04-18 08:30:36 -0700

    Version 1

--- Scripts/Checksum.cs
+++ Scripts/Checksum.cs
@@ -1,11 +1,6 @@
-
 using System;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
-using System.Net;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using Godot;
 
@@ -15,39 +10,74 @@ public static class Checksum
     public static string pathraw;
     public static string hashraw;
     public static string typeraw;
+    public static string lengthraw;
+//cleaned variables
     public static string filepath;
-    public static string hash;
+    public static string check;
     public static string checktype;
+    public static string filehash;
+    public static int hashlength;
 //Outputs
-
+    public static string hash_error;
+    public static string[] file_error;
 //Global Booleans
     public static bool can_run = true;
-
-    public static bool is_base64;
-    public static bool is_base32;
     public static bool is_type_valid;
     public static bool is_check_valid;
+    public static bool is_path_valid;
+    public static bool is_length_valid = true;
+    public static bool is_safe;
+    public static bool is_type_empty;
+    public static bool is_path_empty;
+    public static bool is_hash_empty;
+    public static bool is_length_empty;
+    public static bool is_comparing;
+    public static bool hashed;
 
-    private static string[] hashes = ["MD2", "MD4", "MD5", "SHA1", "SHA256", "SHA384", "SHA512"];
+//Other
 
+//Error Handling
+    private static string path_error;
+
+    private static string[] hashes = ["MD5", "SHA1", "SHA256", "SHA384", "SHA512", "SHA3_256", "SHA3_384", "SHA3_512", "SHAKE128", "SHAKE256"];
+
+    public static void InputCheck(){
+        is_length_valid = true;
+        IsValidSum();
+        IsValidType();
+        IsValidPath();
+        IsValidLength();
+    }
 
 
     public static void Start(){
-        PathCleanup();
-        //Process will run and input will include filepath, checktype, and check
-        UI.Success();
+        filehash = "";
+        hashed = false;
+  
+        Certify();
         can_run = true;
     }
 
-    public static void InputCheck(){
-        IsValidSum();
-        IsValidType();
-    }
 
-
-    public static void PathCleanup(){
+    public static void IsValidPath(){
         filepath = @pathraw.Replace("\"", "");
-    }
+        if(filepath == ""){
+            is_path_empty = true;
+        }
+        else{
+            is_path_empty = false;
+        }
+        try{
+            string stream = File.OpenRead(filepath).ToString();
+            is_path_valid = true;
+            }
+        catch(Exception e){
+            path_error = e.ToString();
+            is_path_valid = false;
+        }
+        file_error = path_error.Split(new string[] {"\r\n", "\r", "\n" }, StringSplitOptions.None);
+        }
+
 //cleans up imperfecions and checks if hashtype is valid
     public static void IsValidType(){
 
@@ -58,6 +88,12 @@ public static class Checksum
         checktype = @checktype.ToUpper();
 
         //confirm validity
+        if(checktype == ""){
+            is_type_empty = true;
+        }
+        else{
+            is_type_empty = false;
+        }
         if(hashes.Contains(checktype)){
             is_type_valid = true;
         }
@@ -67,14 +103,20 @@ public static class Checksum
     }
 
     public static void IsValidSum(){
-        hash = hashraw.Replace("\"", "");
-        string pattern256 = @"^[a-fA-F0-9]{64}$";
-        is_base64 = Regex.IsMatch(hash, pattern256);
-
-        string patternmd5 = @"^[a-fA-F0-9]{32}$";
-        is_base32 = Regex.IsMatch(hash, patternmd5);
-        
-        if(is_base64 || is_base32){
+        check = hashraw.Replace("\"", "");
+        check = hashraw.Replace("-","");
+        if(check == ""){
+            is_hash_empty = true;
+        }
+        else{
+            is_hash_empty = false;
+        }
+        if(Regex.IsMatch(check, @"^[a-zA-Z0-9]+$")){
+            is_check_valid = true;
+            is_comparing = true;
+        }
+        else if(check == "!"){
+            is_comparing = false;
             is_check_valid = true;
         }
         else{
@@ -82,7 +124,277 @@ public static class Checksum
         }
 }
 
+    public static void IsValidLength(){
+        if(checktype == "SHAKE128" || checktype == "SHAKE256"){
+        if(is_comparing){
+            hashlength = check.Length;
+            is_length_valid = true;
+            is_length_empty = false;
+        }
+        else if(lengthraw != ""){
+            is_length_empty = false;
+            lengthraw = lengthraw.Replace(" ", "");
+            if(Regex.IsMatch(lengthraw, @"^\d+$")){
+                hashlength = lengthraw.ToInt();
+                is_length_valid = true;
+            }
+            else{
+                is_length_valid = false;
+            }
+        }
+        else{
+            is_length_empty = true;
+            is_length_valid = false;
+        }
+        }
+    }
+    public static void Certify(){
+        if(checktype == "MD5"){
+            MD5(filepath);
+        }
+        else if(checktype == "SHA1"){
+            SHA1(filepath);
+        }
+        else if(checktype == "SHA256"){
+            SHA256(filepath);
+        }
+        else if(checktype == "SHA384"){
+            SHA384(filepath);
+        }
+        else if(checktype == "SHA3_256"){
+            SHA3_256(filepath);
+        }
+        else if(checktype == "SHA3_384"){
+            SHA3_384(filepath);
+        }
+        else if(checktype == "SHA3_512"){
+            SHA3_512(filepath);
+        }
+        else if(checktype == "SHA512"){
+            SHA512(filepath);
+        }
+        else if(checktype == "SHAKE128"){
+            Shake128(filepath);
+        }
+        else if(checktype == "SHAKE256"){
+            Shake256(filepath);
+        }
+        else{
+            UI.UnknownErrorOccurred();
+        }
 
+    }
+
+
+
+    public static void ChecksumOutput(){
+        if(hashed){
+            if(string.Equals(check, filehash, StringComparison.OrdinalIgnoreCase))
+            {
+                is_safe = true;
+            }
+            else{
+                is_safe = false;
+            }
+            can_run = true;
+        }
+        else{
+            UI.ErrorOccurred();
+        }
+        UI.WriteOutput();
+    }
+
+    public static void MD5(string filename){
+        try{
+        using(var encrypt = System.Security.Cryptography.MD5.Create())
+        {
+            using(var stream = System.IO.File.OpenRead(filename))
+            {
+                var hashtemp = encrypt.ComputeHash(stream);
+                filehash = BitConverter.ToString(hashtemp).Replace("-","");
+            }
+        }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;
+            ChecksumOutput();
+        }
+
+    public static void SHA1(string filename){
+        try{
+        using(var encrypt = System.Security.Cryptography.SHA1.Create())
+        {
+            using(var stream = File.OpenRead(filename))
+            {
+                var hashtemp = encrypt.ComputeHash(stream);
+                filehash = BitConverter.ToString(hashtemp).Replace("-","");
+            }
+        }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;
+            ChecksumOutput();
+        }
+
+    public static void SHA256(string filename){
+        try{
+        using(var encrypt = System.Security.Cryptography.SHA256.Create())
+        {
+            using(var stream = File.OpenRead(filename))
+            {
+                var hashtemp = encrypt.ComputeHash(stream);
+                filehash = BitConverter.ToString(hashtemp).Replace("-","");
+            }
+        }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;
+            ChecksumOutput();
+        }
+
+    public static void SHA384(string filename){
+        try{
+        using(var encrypt = System.Security.Cryptography.SHA384.Create())
+        {
+            using(var stream = System.IO.File.OpenRead(filename))
+            {
+                var hashtemp = encrypt.ComputeHash(stream);
+                filehash = BitConverter.ToString(hashtemp).Replace("-","");
+            }
+        }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;
+            ChecksumOutput();
+        }
+
+    public static void SHA3_256(string filename){
+        try{
+        using(var encrypt = System.Security.Cryptography.SHA3_256.Create())
+        {
+            using(var stream = System.IO.File.OpenRead(filename))
+            {
+                var hashtemp = encrypt.ComputeHash(stream);
+                filehash = BitConverter.ToString(hashtemp).Replace("-","");
+            }
+        }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;    
+            ChecksumOutput();
+        }
+
+    public static void SHA3_384(string filename){
+        try{
+        using(var encrypt = System.Security.Cryptography.SHA3_384.Create())
+        {
+            using(var stream = System.IO.File.OpenRead(filename))
+            {
+                var hashtemp = encrypt.ComputeHash(stream);
+                filehash = BitConverter.ToString(hashtemp).Replace("-","");
+            }
+        }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;
+            ChecksumOutput();
+        }
+
+    public static void SHA3_512(string filename){
+        try{
+        using(var encrypt = System.Security.Cryptography.SHA3_512.Create())
+        {
+            using(var stream = System.IO.File.OpenRead(filename))
+            {
+                var hashtemp = encrypt.ComputeHash(stream);
+                filehash = BitConverter.ToString(hashtemp).Replace("-","");
+            }
+        }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;
+            ChecksumOutput();
+        }
+
+    public static void SHA512(string filename){
+        try{
+        using(var encrypt = System.Security.Cryptography.SHA512.Create())
+        {
+            using(var stream = System.IO.File.OpenRead(filename))
+            {
+                var hashtemp = encrypt.ComputeHash(stream);
+                filehash = BitConverter.ToString(hashtemp).Replace("-","");
+            }
+        }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;
+            ChecksumOutput();
+        }
+
+    public static void Shake128(string filename){
+        try{
+            using(var stream = System.IO.File.OpenRead(filename)){
+                byte[] hashtemp = System.Security.Cryptography.Shake128.HashData(stream,hashlength/2);
+                filehash = BitConverter.ToString(hashtemp).Replace("-", "");
+            }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;
+            ChecksumOutput();
+        }
+
+    public static void Shake256(string filename){
+        try{
+            using(var stream = System.IO.File.OpenRead(filename)){
+                byte[] hashtemp = System.Security.Cryptography.Shake256.HashData(stream,hashlength/2);
+                filehash = BitConverter.ToString(hashtemp).Replace("-", "");
+            }
+        }
+        catch(Exception e)
+        {
+            hash_error = e.ToString();
+            UI.ErrorOccurred();
+        }
+            hashed = true;
+            ChecksumOutput();
+        }
 
 
 }
--- Scripts/UI.cs
+++ Scripts/UI.cs
@@ -1,25 +1,29 @@
 using Godot;
-using System;
-using System.Security.Cryptography.X509Certificates;
-using System.Text.RegularExpressions;
-using System.Xml;
 
 public partial class UI : Control
 {
-	public bool bluh = true;
-
-	public LineEdit Filepath;
-	public LineEdit Hash;
-	public LineEdit Checktype;
-
-	public static RichTextLabel Output;
+//Nodes
+	private static LineEdit Filepath;
+	private static LineEdit Hash;
+	private static LineEdit Checktype;
+	private static RichTextLabel Output;
 	public Timer LoadTime;
+	private static RichTextLabel DebugLabel;
+	public static LineEdit OutputLength;
 
+//strings
 	private static string checkinfo;
 	private static string typeinfo;
+	private static string pathinfo;
+	private static string lengthinfo;
+	private static string greeting;
+//colors
+	private static Color error = new Color(1,0,0,1);
+	private static Color red = new Color(1,0,0,1);
+	private static Color green = new Color(0,1,0,1);
 
 
-	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready()
 	{
 		Filepath = GetNode<LineEdit>("/root/UI/Control/VBoxContainer/Filepath");
@@ -27,62 +31,127 @@ public partial class UI : Control
 		Output = GetNode<RichTextLabel>("/root/UI/Control/VBoxContainer/RichTextLabel");
 		Checktype = GetNode<LineEdit>("/root/UI/Control/VBoxContainer/Hashtype");
 		LoadTime = GetNode<Timer>("/root/UI/LoadTime");
+		DebugLabel = GetNode<RichTextLabel>("/root/UI/DebugLabel");
+		OutputLength = GetNode<LineEdit>("/root/UI/Control/VBoxContainer/OutputLength");
+
+		LoadTime.Timeout += OnTimerTimeoutSignal;
+		greeting = "Tips: \n[*] Filepath should be from root\n[*] To run with no hashcheck, input ! as the hash \n[*] Output Length is only used for SHAKE algorithms with no check\n[*] Press Enter to Run";
+		Output.Text = string.Format(greeting);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		DebugLabel.Text = "";
+		DebugLabel.AddText(string.Format("is_length_valid?: {0} \n", Checksum.is_length_valid));
+		DebugLabel.AddText(string.Format("filehash: {0} \n", Checksum.filehash));
+		DebugLabel.AddText(string.Format("can_run: {0} \n", Checksum.can_run));
+		DebugLabel.AddText(string.Format("load time: {0} \n", LoadTime.TimeLeft.ToString()));
+		DebugLabel.AddText(string.Format("can_run: {0} \n", Checksum.is_safe));
+		DebugLabel.AddText(string.Format("is_check_valid? {0}\n", Checksum.is_check_valid.ToString()));
+		DebugLabel.AddText(string.Format("check value: {0} \n", Checksum.check));
+		DebugLabel.AddText(string.Format("is_comparing?: {0} \n", Checksum.is_comparing));
+
 		if(Input.IsActionJustPressed("Enter") && Checksum.can_run){
 			Checksum.can_run = false;
 
 			Checksum.typeraw = Checktype.Text;
 			Checksum.hashraw = Hash.Text;
 			Checksum.pathraw = Filepath.Text;
+			Checksum.lengthraw = OutputLength.Text;
 
 
 			Checksum.InputCheck();
 
-			if(Checksum.is_check_valid && Checksum.is_type_valid){
+			if(Checksum.is_check_valid && Checksum.is_type_valid && Checksum.is_path_valid && Checksum.is_length_valid){
 				Checksum.Start();
 			}
-			else{InvalidHandler();}
+			else{
+				InvalidHandler();
+				}
 		}
 	}
 
 	private void InvalidHandler(){
 		Output.Text = "";
-
-		if(Checksum.is_check_valid){
-		checkinfo = "";
-		}
-		else{
-			checkinfo = string.Format("Checksum: invalid, please re-enter");
-		}
-
-		if(Checksum.is_type_valid){
-			typeinfo = "";
-		}
-		else{
-			typeinfo = string.Format("Checktype: invalid, please re-enter /nsupported checktypes: MD2 MD4 MD5 SHA1 SHA256 SHA384 SHA512");
-		}
+		pathinfo = string.Format("Filepath Exception Occurred:");
+		checkinfo = string.Format("Invalid hash. to run with no hashcheck, enter ! for hash");
+		typeinfo = string.Format("Invalid checktype\n(supported checktypes: MD5, SHA1, SHA256, SHA384, SHA512, SHA3_256, SHA3_384, SHA3_512, Shake128, Shake256)");
+		lengthinfo = string.Format("Invalid output length (note: SHAKE will always output an even number, rounded down)");
+		LoadTime.Start();
+	}
+	private void OnTimerTimeoutSignal(){
 		InvalidOutput();
 	}
 	private void InvalidOutput(){
-		if(!Checksum.is_check_valid && !Checksum.is_type_valid){
-		Output.Text = string.Format("{0} \n{1}", checkinfo, typeinfo);
+		if(!Checksum.is_path_valid){
+			if(Checksum.is_path_empty){
+				Output.AddText("> No Filepath provided\n\n");
+			}
+			else{
+				Output.AddText(string.Format("! {0}\n  {1}\n\n", pathinfo, Checksum.file_error[0]));
+			}
 		}
-		else if(!Checksum.is_check_valid){
-			Output.Text = string.Format(checkinfo);
+		if(!Checksum.is_type_valid){
+			if(Checksum.is_type_empty){
+				Output.AddText("> No checktype provided\n\n");
+			}
+			else{
+				Output.AddText(string.Format("> {0}\n\n", typeinfo));
+			}
 		}
-		else{
-			Output.Text = string.Format(typeinfo);
+		if(!Checksum.is_check_valid){
+			if(Checksum.is_hash_empty){
+				Output.AddText("> No hash provided. To run with no hashcheck, enter ! for hash\n\n");
+			}
+			else{
+				Output.AddText(string.Format("> {0}\n\n", checkinfo));
+			}
 		}
+		if(!Checksum.is_length_valid){
+			if(Checksum.is_length_empty){
+				Output.AddText("> No Output Length Provided (note: SHAKE will always round to an even number)");
+			}
+			else{
+			Output.AddText(string.Format("> {0}\n\n", lengthinfo));
+			}
+		}
+
 		Checksum.can_run = true;
 	}
 
+	public static void WriteOutput(){
+		if(Checksum.is_comparing){
+			Output.Text = "";
+			Output.Text = string.Format("Extracted hash: {0}, Compared Hash: {1}\n\n", Checksum.filehash, Checksum.check.ToUpper());
+			Output.AddText(string.Format("is file safe? "));
+			if(Checksum.is_safe){
+				Output.PushColor(green);
+				Output.AddText("YES");
+			}
+			else{
+				Output.PushColor(red);
+				Output.AddText("NO");
+			}
+		}
 
-	public static void Success(){
-		Output.Text = "you did it ayayaya :3";
+		else{
+			Output.Text = "";
+			Output.Text = string.Format("Extracted hash: {0}, of type {1}\n", Checksum.filehash, Checksum.checktype);
+		}
+
+	}
+
+
+	public static void ErrorOccurred(){
+		Output.Text = "";
+		Output.AddText("Hashing error occurred!");
+		Output.PushColor(error);
+		Output.AddText(Checksum.hash_error);
+		Checksum.can_run = true;
+	}
+
+	public static void UnknownErrorOccurred(){
+		Output.Text = "A completely unkown error occurred... please write up a report on github so I can try to fix";
 	}
 }
 
